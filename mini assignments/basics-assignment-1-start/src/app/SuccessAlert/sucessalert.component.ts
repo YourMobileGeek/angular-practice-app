1c948be8b75d5b58d18367af0de677b421c1d70a
Ariana Davis <arianad@Arianas-MacBook-Pro.local>
2018-08-09 16:33:28 -0400

    basics assignment 1 completion

---
+++ mini assignments/basics-assignment-1-start/src/app/SuccessAlert/sucessalert.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({ 
+    selector: 'app-success-alert', 
+    templateUrl: './sucessalert.component.html',
+    styleUrls: ['./successalert.component.css']
+ })
+
+export class SuccessAlertComponent implements OnInit { 
+
+    constructor() {}
+
+    ngOnInit() {
+
+    }
+    
+ }
