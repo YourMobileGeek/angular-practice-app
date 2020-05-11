d2d99135ec386bb4ab39f1f9eca0e38e108a7147
Ariana Davis <adavis@warriors.com>
2020-05-11 00:35:28 -0700

    updated Angular practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+
 @Component({
+
+  // assigns a string a value s
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
 }
