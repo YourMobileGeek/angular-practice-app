1d24a05429eb3ef1309023a62cd9813412c9dcd9
Ariana Davis <adavis@warriors.com>
2020-05-12 23:37:35 -0700

    updated angular practice

--- mini assignments/basics-assignment-3-solution/src/app/app.component.ts
+++ mini assignments/basics-assignment-3-solution/src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-
-  // assigns a string a value s
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  showSecret = false; 
+  showSecret = false;
   log = [];
 
   onToggleDetails() {
-      this.showSecret = !this.showSecret; 
-      // this.log.push(this.log.length + 1);
-      this.log.push(new Date());
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,7 +10,9 @@ export class ServersComponent implements OnInit {
 
     allowNewServer = false; 
     serverCreationStatus = 'No server was created!'; 
-    serverName = 'Testserver';
+    serverName = 'Testing';
+    serverCreated = false; 
+    servers = ['Testserver', 'Testserver2'];
 
     constructor() {
         
@@ -25,7 +27,9 @@ export class ServersComponent implements OnInit {
     }
 
     onCreateServer() {
-        this.serverCreationStatus = 'Server was created! Name is' + "  " + this.serverName;
+        this.serverCreated = true; 
+        this.servers.push(this.serverName);
+        this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
     }
 
     onUpdateServerName(event: Event) {
